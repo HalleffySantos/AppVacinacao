4674fc1f8d77f9f1e581fa357d981b775a3e7f36
Halleffy Santos <78797938+HalleffySantos@users.noreply.github.com>
2022-04-04 19:52:57 -0300

    Edição IDs Front End

--- yo/src/main/java/Servlets/Vacinacao.java
+++ yo/src/main/java/Servlets/Vacinacao.java
@@ -1,17 +1,11 @@
 package Servlets;
 
 import java.io.IOException;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import Negocio.Vacinas;
-import Persistencia.Vacinacao.IPersistenciaVacinas;
-import Persistencia.Vacinacao.PersistenciaVacinas;
 
 /**
  * Servlet implementation class Vacinacao
@@ -32,10 +26,7 @@ public class Vacinacao extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		IPersistenciaVacinas persistenciaVacina = new PersistenciaVacinas();
-		List<Vacinas> vacinas = persistenciaVacina.GetVacinas();
-		
+		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
